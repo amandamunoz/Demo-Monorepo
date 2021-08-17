f481a87bc33c37afd8b69dcfd2cbdb85575e0cee
Amanda Munoz <amanda@ideas42ventures.com>
2021-08-17 16:36:39 -0400

    Remove client and server

--- server/app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::API
-  def index
-    render :json => {:message => "If you see this message, I come from the server. I am alive!"}
-  end
-end
--- server/db/migrate/20210817014801_create_pets.rb
+++
@@ -1,8 +0,0 @@
-class CreatePets < ActiveRecord::Migration[6.1]
-  def change
-    create_table :pets do |t|
-      t.string :name
-      t.timestamps
-    end
-  end
-end
